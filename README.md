55463a5ca95b3549af1bbb5e9e669495ae325ceb
gridle <onrust.karlijn@gmail.com>
2018-03-24 13:12:32 +0100

    Readme
    Co-Authored-By: Karlijn Onrust <onrust.karlijn@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # My-first-app
+This is my first tryout of Android studio. Never made an app before. I desided what I wanted to learn and teached myself using google and tutorialpoint. 
+My learning goals were:
+    - Finding out how to make a new activity
+    - Making a backbutton in actionbar
+    - insert images
+    - Linking to other apps
+    - Insert, read and delete from a database (no luck)
--- app/src/main/java/com/example/onrus/myapplication/Home.java
+++ app/src/main/java/com/example/onrus/myapplication/Home.java
@@ -1,9 +1,9 @@
 package com.example.onrus.myapplication;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
 
 public class Home extends AppCompatActivity {
 
